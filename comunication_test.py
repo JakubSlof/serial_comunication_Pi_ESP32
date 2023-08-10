e81365f40fa8fdba3b8beac285f982b749422bb8
Jakub Slof <jakub.sloff@gmail.com>
2023-08-10 09:45:10 +0200

    ooprava komentu

--- comunication_test.py
+++ comunication_test.py
@@ -3,7 +3,7 @@
 import serial
 import time 
 
-port = 'COM3' #nazev portu kde je esp32 pripojeno 
+port = 'COM3' #nazev portu kde je esp32 pripojeno, portse musi zmenit pri zmene portu 
 baund_rate = 115200 # rychlost seriove komunikace 
 ser = serial.Serial(port, baund_rate,timeout=1) # nastaveni parametru komunikace 
 time.sleep(3)#cas nez se esp zapne a zacne fungovat 
